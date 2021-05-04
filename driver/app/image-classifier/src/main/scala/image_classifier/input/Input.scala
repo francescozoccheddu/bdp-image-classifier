f0d2acdfd29d536f73d4ac584cf7204b0068aa58
Francesco Zoccheddu <23437496+francescozoccheddu@users.noreply.github.com>
2021-05-04 21:23:58 +0200

    Add load code

--- driver/app/image-classifier/src/main/scala/image_classifier/input/Input.scala
+++ driver/app/image-classifier/src/main/scala/image_classifier/input/Input.scala
@@ -1,27 +1,49 @@
 package image_classifier.input
 
-import org.apache.spark.rdd.RDD
-import org.apache.spark.ml.image.ImageSchema
 import org.apache.spark.SparkContext
-import com.ctc.wstx.cfg.InputConfigFlags
 import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.DataFrame
+import org.apache.spark.sql.functions.lit
 
-case class Input private (options : InputOptions, data : RDD[ImageSchema], classes : List[String]) {
-
+case class Input private (options : InputOptions, data : DataFrame, classes : List[String]) {
+    
     require(classes.nonEmpty)
-
+    
 }
 
 object Input {
-
+    
+    val isTestColumn : String = "isTest"
+    val classColumn : String = "class"
+    
     def loadFromConfigFile(configFile : String, spark : SparkSession) : Input = {
-        load(InputConfiguration.fromFile(configFile, spark))
+        load(InputConfiguration.fromFile(configFile), spark)
     }
 
     private[input] def load(config : InputConfiguration, spark : SparkSession) : Input = {
-        
+        val classesData = for ((c, i) <- config.classes.zipWithIndex) yield loadClass(config, c, spark).withColumn(classColumn, lit(i))
+        val data = classesData.reduce(_ union _)
+        val classNames = config.classes.map(_.name).toList
+        Input(config.toOptions, data, classNames)
+    }
+    
+    private def loadClass(config : InputConfiguration, classConfig : InputClass, spark : SparkSession) : DataFrame = {
+        val trainImages = loadImages(classConfig.trainFiles, spark)
+        val testImages = loadImages(classConfig.testFiles, spark)
+        val mergedImages = loadImages(classConfig.mergedFiles, spark)
+        val testSeed = config.testSeed.getOrElse(util.Random.nextInt)
+        val Array(mergedTrainImages, mergedTestImages) = mergedImages.randomSplit(Array(1.0 - config.testFraction, config.testFraction), testSeed)
+        mergeImages(trainImages union mergedTrainImages, testImages union mergedTestImages)
+    }
+    
+    private def mergeImages(trainImages : DataFrame, testImages : DataFrame) : DataFrame = {
+        trainImages.withColumn(isTestColumn, lit(false))
+        testImages.withColumn(isTestColumn, lit(true))
+        trainImages union testImages
     }
 
-    
+    private def loadImages(paths : Seq[String], spark : SparkSession) : DataFrame = {
+        spark.read.format("image").option("dropInvalid", true).load(paths : _*)
+    }
 
 }
