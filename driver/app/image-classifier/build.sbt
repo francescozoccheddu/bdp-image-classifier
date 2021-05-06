e379e21475764c39fbc22462249a331890d471bd
Francesco Zoccheddu <23437496+francescozoccheddu@users.noreply.github.com>
2021-05-06 18:45:10 +0200

    Fix loading

--- driver/app/image-classifier/build.sbt
+++ driver/app/image-classifier/build.sbt
@@ -1,10 +1,13 @@
 
 scalaVersion := "2.12.13"
 
+fork := true
+
 libraryDependencies ++= Seq(
 	"org.apache.spark" %% "spark-core" % "3.1.1",
 	"org.apache.spark" %% "spark-sql" % "3.1.1",
 	"org.apache.spark" %% "spark-mllib" % "3.1.1",
-	"com.github.pathikrit" %% "dijon" % "0.3.0"
+  	"com.github.plokhotnyuk.jsoniter-scala" %% "jsoniter-scala-core"   % "2.8.0",
+  	"com.github.plokhotnyuk.jsoniter-scala" %% "jsoniter-scala-macros" % "2.8.0" % "compile-internal"
 )
 
--- driver/app/image-classifier/src/main/scala/image_classifier/input/InputClass.scala
+++ driver/app/image-classifier/src/main/scala/image_classifier/input/InputClass.scala
@@ -4,7 +4,7 @@ import org.apache.spark.sql.SparkSession
 import org.apache.spark.rdd.RDD
 import org.apache.spark.ml.image.ImageSchema
 
-private[input] case class InputClass(name : String, trainFiles : List[String], testFiles : List[String], mergedFiles : List[String]) {
+private[input] case class InputClass(name : String, trainFiles : Seq[String] = Seq[String](), testFiles : Seq[String] = Seq[String](), mergedFiles : Seq[String] = Seq[String]()) {
 
 	require(name.trim.nonEmpty && name.length < 128)
 
