39bc9fafe701dcc51181477410341295028086c6
Francesco Zoccheddu <23437496+francescozoccheddu@users.noreply.github.com>
2021-05-10 17:50:42 +0200

    Update NearestNeighbor

--- driver/app/image-classifier/src/main/scala/image_classifier/Pipeline.scala
+++ driver/app/image-classifier/src/main/scala/image_classifier/Pipeline.scala
@@ -25,23 +25,23 @@ object Pipeline {
 		val descriptorFactory = DescriptorFactory(input.options.localFeaturesCount, input.options.maxImageSize)
 		val describeUdf = udf((row : Row) => descriptorFactory.describe(ImageSchema.getWidth(row), ImageSchema.getHeight(row), ImageSchema.getMode(row), ImageSchema.getData(row)))
 		input.data
-			.withColumn(entryColName, monotonically_increasing_id)
-			.withColumn(dataColName, explode(describeUdf(dataCol)))
+		.withColumn(entryColName, monotonically_increasing_id)
+		.withColumn(dataColName, explode(describeUdf(dataCol)))
 	}
 	
 	private def createCodebook(spark : SparkSession, size : Int, data : DataFrame, assignNearest : Boolean = true) : DataFrame = {
 		import org.apache.spark.ml.clustering.KMeans
+		import org.apache.spark.sql.functions.monotonically_increasing_id
 		import spark.implicits._
 		val trainSet = data.filter(!isTestCol)
 		val model = new KMeans().setK(size).setMaxIter(200).setFeaturesCol(dataColName).fit(trainSet)
 		val centers = model.clusterCenters.toSeq.map(Tuple1(_)).toDF(dataColName)
-		centers.show()
 		if (assignNearest) 
 			NearestNeighbor.join(spark, trainSet, centers).select(NearestNeighbor.neighborCol.alias(dataColName))
 		else 
 			centers
 	}
-	
+
 	def run(spark : SparkSession,input : Input) {
 		val data = describe(spark, input).cache
 		val codebook = createCodebook(spark, input.options.codebookSize, data, input.options.codebookAssignNearest).cache
