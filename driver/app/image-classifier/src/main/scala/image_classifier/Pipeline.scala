83f16306fa1386ea1992974c753d87a49cbc0b12
Francesco Zoccheddu <23437496+francescozoccheddu@users.noreply.github.com>
2021-05-10 20:30:44 +0200

    Separate dataframes

--- driver/app/image-classifier/src/main/scala/image_classifier/Pipeline.scala
+++ driver/app/image-classifier/src/main/scala/image_classifier/Pipeline.scala
@@ -21,52 +21,57 @@ object Pipeline {
 	val neighborCol = col(neighborColName)
 	private val entryCol = col(entryColName)
 	
-	private def describe(spark : SparkSession, input : Input) : DataFrame = {
+	private def describe(spark : SparkSession, data : DataFrame, localFeaturesCount : Int, maxImageSize : Int) : DataFrame = {
 		import image_classifier.features.DescriptorFactory
 		import org.apache.spark.sql.functions.{udf, monotonically_increasing_id, explode}
 		import org.apache.spark.ml.image.ImageSchema
 		import spark.implicits._
-		val descriptorFactory = DescriptorFactory(input.options.localFeaturesCount, input.options.maxImageSize)
+		val descriptorFactory = DescriptorFactory(localFeaturesCount, maxImageSize)
 		val describeUdf = udf((row : Row) => descriptorFactory.describe(ImageSchema.getWidth(row), ImageSchema.getHeight(row), ImageSchema.getMode(row), ImageSchema.getData(row)))
-		input.data
-			.withColumn(entryColName, monotonically_increasing_id)
-			.withColumn(dataColName, explode(describeUdf(dataCol)))
+		data.withColumn(dataColName, explode(describeUdf(dataCol)))
 	}
 	
 	private def createCodebook(spark : SparkSession, size : Int, data : DataFrame, assignNearest : Boolean = true) : DataFrame = {
 		import org.apache.spark.ml.clustering.KMeans
-		import org.apache.spark.sql.functions.{monotonically_increasing_id, explode}
+		import org.apache.spark.sql.functions.explode
 		import spark.implicits._
-		val trainSet = data.filter(!isTestCol)
-		val model = new KMeans().setK(size).setMaxIter(200).setFeaturesCol(dataColName).fit(trainSet)
+		val model = new KMeans().setK(size).setMaxIter(200).setFeaturesCol(dataColName).fit(data)
 		val centers = model.clusterCenters.toSeq.map(Tuple1(_)).toDF(dataColName)
 		if (assignNearest) 
-			NearestNeighbor.join(spark, trainSet, centers, Seq(dataColName))
+			NearestNeighbor.join(spark, data, centers, Seq(dataColName))
 				.select(neighborCol.getField(dataColName).alias(dataColName))
 		else 
 			centers
 	}
 	
-	private def computeBOFS(spark : SparkSession, data : DataFrame, codebook : DataFrame) : DataFrame = {
-		val matches = {
-			val indexedCodebook = {
-				import org.apache.spark.sql.functions.{monotonically_increasing_id, row_number, lit}
-				import org.apache.spark.sql.expressions.Window
-				val window = Window.orderBy(monotonically_increasing_id).partitionBy(lit(0))
-				codebook.withColumn(entryColName, row_number.over(window).cast("long"))
-			}
-			val joint = NearestNeighbor.join(spark, indexedCodebook, data, Seq(entryColName))
-				.select(entryCol, neighborCol.getField(entryColName).alias(neighborColName))
-			joint.join(data.drop(dataCol).distinct, entryColName)
+	private def matchFeatures(spark : SparkSession, data : DataFrame, codebook : DataFrame) : DataFrame = {
+		val indexedCodebook = {
+			import org.apache.spark.sql.functions.{monotonically_increasing_id, row_number, lit}
+			import org.apache.spark.sql.expressions.Window
+			val window = Window.orderBy(monotonically_increasing_id).partitionBy(lit(0))
+			codebook.withColumn(entryColName, row_number.over(window).cast("long"))
 		}
-		matches.sample(0.1).print("Matches")
-		???
+		NearestNeighbor.join(spark, indexedCodebook, data, Seq(entryColName))
+			.select(entryCol, neighborCol.getField(entryColName).alias(neighborColName))
 	}
 
+
 	def run(spark : SparkSession,input : Input) {
-		val data = describe(spark, input).cache
-		val codebook = createCodebook(spark, input.options.codebookSize, data, input.options.codebookAssignNearest).cache
-		val bofs = computeBOFS(spark, data, codebook)
+		val (legend, matches) = {
+			val indexedInput = {
+				import org.apache.spark.sql.functions.monotonically_increasing_id
+				input.data.withColumn(entryColName, monotonically_increasing_id)
+			} 
+			val features = describe(spark, indexedInput, input.options.localFeaturesCount, input.options.maxImageSize).cache
+			val codebook = createCodebook(spark, input.options.codebookSize, features.filter(!isTestCol), input.options.codebookAssignNearest).cache
+			val matches = matchFeatures(spark, features, codebook).cache
+			codebook.unpersist
+			val legend = features.select(entryCol, isTestCol, classCol).cache
+			features.unpersist
+			(legend, matches)
+		} 
+		legend.print("Legend")
+		matches.print("Matches")
 	}
 
 }
---
+++ driver/app/image-classifier/src/main/scala/image_classifier/utils/Histogram.scala
@@ -0,0 +1,14 @@
+package image_classifier.utils
+
+import org.apache.spark.rdd.RDD
+import org.apache.spark.ml.linalg.{Vector => MLVector}
+
+object Histogram {
+
+	def compute(data : RDD[Int], min : Int, max : Int) : MLVector = {
+
+
+		???
+	}
+
+}
