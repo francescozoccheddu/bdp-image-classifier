e96cbb4252fa8426f4f50937e4f54762b629280d
Francesco Zoccheddu <23437496+francescozoccheddu@users.noreply.github.com>
2021-05-10 19:06:41 +0200

    Fix matching

---
+++ driver/app/image-classifier/src/main/scala/image_classifier/utils/DataFrameImplicits.scala
@@ -0,0 +1,18 @@
+package image_classifier.utils
+
+import org.apache.spark.sql.DataFrame
+
+object DataFrameImplicits {
+
+	implicit class DataFrameExtension(df : DataFrame) {
+
+		def print(name : String = "DataFrame") = {
+			println()
+			println(s"${name}[${df.count}]:")
+			df.printSchema()
+			df.show(20, 100)
+		}
+		
+	}
+
+}
