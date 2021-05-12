9f7dd2564037923cf11dda56d9ddcb798647c447
Francesco Zoccheddu <23437496+francescozoccheddu@users.noreply.github.com>
2021-05-13 00:01:00 +0200

    Update Config

--- driver/app/image-classifier/build.sbt
+++ driver/app/image-classifier/build.sbt
@@ -7,8 +7,8 @@ libraryDependencies ++= Seq(
 	"org.apache.spark" %% "spark-core" % "3.1.1",
 	"org.apache.spark" %% "spark-sql" % "3.1.1",
 	"org.apache.spark" %% "spark-mllib" % "3.1.1",
-  	"com.github.plokhotnyuk.jsoniter-scala" %% "jsoniter-scala-core"   % "2.8.0",
-  	"com.github.plokhotnyuk.jsoniter-scala" %% "jsoniter-scala-macros" % "2.8.0" % "compile-internal"
-)
+	"com.github.plokhotnyuk.jsoniter-scala" %% "jsoniter-scala-core" % "2.8.0",
+	"com.github.plokhotnyuk.jsoniter-scala" %% "jsoniter-scala-macros" % "2.8.0" % "compile"
+	)
 
 // TODO Setup for spark-submit
--- driver/app/image-classifier/src/main/scala/image_classifier/configuration/ConfigUtils.scala
+++ driver/app/image-classifier/src/main/scala/image_classifier/configuration/ConfigUtils.scala
@@ -1,42 +1,59 @@
 package image_classifier.configuration
 
-import com.github.plokhotnyuk.jsoniter_scala.core.{JsonValueCodec, JsonReader, JsonWriter}
-import com.github.plokhotnyuk.jsoniter_scala.macros.{JsonCodecMaker, CodecMakerConfig}
-
 private[configuration] object ConfigUtils {
-	
-	private implicit val loadCodec: JsonValueCodec[Load] = new JsonValueCodec[Load] {
-		
-		override def decodeValue(reader: JsonReader, default: Load): Load = Load(reader.readString(null))
-		
-		override def encodeValue(value: Load, writer: JsonWriter): Unit = writer.writeVal(value.file)
-		
-		override val nullValue: Load = null
-		
+
+	import com.github.plokhotnyuk.jsoniter_scala.core.JsonValueCodec
+	import com.github.plokhotnyuk.jsoniter_scala.macros.{CodecMakerConfig, JsonCodecMaker}
+
+	private def makeCodec[LoadableType >: Null <: Loadable[LoadableType]]: JsonValueCodec[LoadableOrLoad[LoadableType]] = new JsonValueCodec[LoadableOrLoad[LoadableType]] {
+
+		import com.github.plokhotnyuk.jsoniter_scala.core.{JsonReader, JsonWriter}
+
+		private val defaultCodec: JsonValueCodec[LoadableType] = JsonCodecMaker.make(CodecMakerConfig.withSkipUnexpectedFields(false))
+
+		override def decodeValue(reader: JsonReader, default: LoadableOrLoad[LoadableType]): LoadableOrLoad[LoadableType] = {
+			if (reader.isNextToken('"'))
+				Load(reader.readString(null))
+			else
+				defaultCodec.decodeValue(reader, null)
+		}
+
+		override def encodeValue(value: LoadableOrLoad[LoadableType], writer: JsonWriter): Unit = value match {
+			case Load(file) => writer.writeVal(file)
+			case loadable: LoadableType => defaultCodec.encodeValue(loadable, writer)
+		}
+
+		override def nullValue: LoadableOrLoad[LoadableType] = null
+
 	}
 
-	private implicit val loadableCodec: JsonValueCodec[] = JsonCodecMaker.make
-	
+	private implicit val splitDataSetCodec = makeCodec[SplitDataSetConfig]
+	private implicit val jointDataCodec = makeCodec[JointDataConfig]
+	private implicit val featurizationCodec = makeCodec[FeaturizationConfig]
+	private implicit val trainingCodec = makeCodec[TrainingConfig]
+
 	private implicit val codec: JsonValueCodec[Config] = JsonCodecMaker.make(CodecMakerConfig.withSkipUnexpectedFields(false))
-	
-	def toFile(config : Config, file : String): Unit = {
-		import java.io.File
+
+	def toFile(config: Config, file: String): Unit = {
 		import org.apache.commons.io.FileUtils
-		FileUtils.writeStringToFile(new File(file), toJson(config))
+		import java.io.File
+		import java.nio.charset.Charset
+		FileUtils.writeStringToFile(new File(file), toJson(config), Charset.defaultCharset())
 	}
-	
-	def toJson(config : Config): String = {
+
+	def toJson(config: Config): String = {
 		import com.github.plokhotnyuk.jsoniter_scala.core.writeToString
 		writeToString(config)
 	}
 
-	def fromFile(file : String) : Config = {
-		import java.io.File
+	def fromFile(file: String): Config = {
 		import org.apache.commons.io.FileUtils
-		fromJson(FileUtils.readFileToString(new File(file)))
+		import java.io.File
+		import java.nio.charset.Charset
+		fromJson(FileUtils.readFileToString(new File(file), Charset.defaultCharset()))
 	}
 
-	def fromJson(json : String) : Config = {
+	def fromJson(json: String): Config = {
 		import com.github.plokhotnyuk.jsoniter_scala.core.readFromString
 		readFromString[Config](json)
 	}
