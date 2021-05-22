6f3fbda1b94f7eb4729f48f20f5452fc85729a0d
Francesco Zoccheddu <23437496+francescozoccheddu@users.noreply.github.com>
2021-05-22 17:06:46 +0200

    Minor fixes

--- driver/app/image-classifier/src/main/scala/image_classifier/launch/Launcher.scala
+++ driver/app/image-classifier/src/main/scala/image_classifier/launch/Launcher.scala
@@ -1,11 +1,17 @@
 package image_classifier.launch
 
 object Launcher {
-	import org.apache.log4j.{BasicConfigurator, Logger}
-
-	BasicConfigurator.configure()
+	{
+		import org.apache.log4j.{BasicConfigurator, Logger}
+		BasicConfigurator.configure()
+		import org.apache.log4j.Level
+		Logger.getRootLogger.setLevel(Level.INFO)
+		Logger.getLogger("org").setLevel(Level.ERROR)
+		Logger.getLogger("com").setLevel(Level.ERROR)
+	}
 
 	import image_classifier.configuration.Config
+	import org.apache.log4j.Logger
 
 	private val logger = Logger.getLogger(Launcher.getClass)
 
@@ -17,11 +23,9 @@ object Launcher {
 	}
 
 	def run(config: Config, workingDir: String): Unit = {
-		import image_classifier.pipeline.Pipeline
+
 		import image_classifier.utils.SparkInstance
-		import org.apache.log4j.Level
-		Logger.getRootLogger.setLevel(Level.INFO)
-		Logger.getLogger("org").setLevel(Level.ERROR)
+		import image_classifier.pipeline.Pipeline
 		SparkInstance.execute(Pipeline.run(config, workingDir)(_))
 	}
 
