4d6bfe4d28aff6949e853b16d4b869a5169bd2bf
Francesco Zoccheddu <23437496+francescozoccheddu@users.noreply.github.com>
2021-05-23 08:55:12 +0200

    Add naive NearestNeighbor

--- driver/app/image-classifier/src/main/scala/image_classifier/pipeline/utils/NearestNeighbor.scala
+++ driver/app/image-classifier/src/main/scala/image_classifier/pipeline/utils/NearestNeighbor.scala
@@ -4,14 +4,39 @@ import image_classifier.pipeline.utils.NearestNeighbor.defaultOutputCol
 import org.apache.spark.ml.linalg.{Vector => MLVector}
 
 private[pipeline] final class NearestNeighbor(inputCol: String, outputCol: String = defaultOutputCol) {
-	import image_classifier.pipeline.utils.Columns.resColName
-	import org.apache.spark.sql.{DataFrame, Row}
+	import org.apache.spark.sql.{DataFrame, Dataset, Encoder, Row}
 
 	import reflect.runtime.universe.TypeTag
+	import scala.reflect.ClassTag
 
-	def naiveJoin[T](key: DataFrame, test: Seq[T], map: T => MLVector)(implicit tag: TypeTag[T]) = {
+	def join[T](key: DataFrame, test: DataFrame)(implicit tag: TypeTag[T], classTag: ClassTag[T]): DataFrame =
+		join(key, test, inputCol)
+
+	def join[T](key: DataFrame, test: DataFrame, testInputCol: String)(implicit tag: TypeTag[T], classTag: ClassTag[T]): DataFrame =
+		join(key, test, (t: DataFrame) => t.rdd.map(_.getAs[T](0)).collect.toSeq, testInputCol)
+
+	def join[T](key: DataFrame, test: DataFrame, map: Row => T)(implicit tag: TypeTag[T], classTag: ClassTag[T]): DataFrame =
+		join(key, test, map, inputCol)
+
+	def join[T](key: DataFrame, test: DataFrame, map: Row => T, testInputCol: String)(implicit tag: TypeTag[T], classTag: ClassTag[T]): DataFrame =
+		join(key, test, (t: DataFrame) => t.rdd.map(map).collect.toSeq, testInputCol)
+
+	private def join[T](key: DataFrame, test: DataFrame, map: DataFrame => Seq[T], testInputCol: String)(implicit tag: TypeTag[T]): DataFrame = {
+		import org.apache.spark.sql.functions.col
+		val distinctTest = test.dropDuplicates(testInputCol).cache
+		val testData = map(distinctTest)
+		val testFeatures = distinctTest.select(col(inputCol)).rdd.map(_.getAs[MLVector](0)).collect
+		join(key, testData, testFeatures)
+	}
+
+	def join[T](key: DataFrame, test: Dataset[T], map: T => MLVector)(implicit tag: TypeTag[T]): DataFrame =
+		join(key, test.collect(), map)
+
+	def join[T](key: DataFrame, test: Seq[T], map: T => MLVector)(implicit tag: TypeTag[T]): DataFrame =
+		join(key, test, test.map(map))
+
+	def join[T](key: DataFrame, test: Seq[T], testFeatures: Seq[MLVector])(implicit tag: TypeTag[T]): DataFrame = {
 		import org.apache.spark.sql.functions.{col, udf}
-		val testFeatures = test.map(map)
 		val mapper = udf[T, MLVector]((f: MLVector) => {
 			var minDist: Double = Double.PositiveInfinity
 			var minI = 0
@@ -25,21 +50,6 @@ private[pipeline] final class NearestNeighbor(inputCol: String, outputCol: Strin
 			}
 			test(minI)
 		})
-		key.withColumn(outputCol, mapper(col(inputCol)))
-	}
-
-	private val hashCol = resColName("hash")
-
-	def approxJoin[T](key: DataFrame, test: DataFrame, map: Row => T)(implicit tag: TypeTag[T]) = {
-		import org.apache.spark.sql.functions.{col, udf}
-		import org.apache.spark.ml.feature.BucketedRandomProjectionLSH
-		val model = new BucketedRandomProjectionLSH()
-			.setBucketLength(3.0)
-			.setNumHashTables(5)
-			.setInputCol(inputCol)
-			.setOutputCol(hashCol)
-			.fit(test)
-		val mapper = udf((f: MLVector) => map(model.approxNearestNeighbors(test, f, 1).collect.head.asInstanceOf[Row]))
 		key.withColumn(outputCol, mapper(col(inputCol)))
 	}
 
