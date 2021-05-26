2259ef1e98d9948628127cd78692289e420c8bec
Francesco Zoccheddu <23437496+francescozoccheddu@users.noreply.github.com>
2021-05-26 22:21:52 +0200

    Fix HDFS issues

--- app/image-classifier/src/main/scala/image_classifier/launch/Launcher.scala
+++ app/image-classifier/src/main/scala/image_classifier/launch/Launcher.scala
@@ -1,15 +1,6 @@
 package image_classifier.launch
 
 object Launcher {
-	{
-		import org.apache.log4j.{BasicConfigurator, Logger}
-		BasicConfigurator.configure()
-		import org.apache.log4j.Level
-		Logger.getRootLogger.setLevel(Level.INFO)
-		Logger.getLogger("org").setLevel(Level.ERROR)
-		Logger.getLogger("com").setLevel(Level.ERROR)
-		Logger.getLogger("breeze").setLevel(Level.ERROR)
-	}
 
 	import image_classifier.configuration.Config
 
