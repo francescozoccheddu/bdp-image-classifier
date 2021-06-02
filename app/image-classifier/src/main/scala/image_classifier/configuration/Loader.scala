f8814a6d7a87b66a702a73e3394558df2b4de912
Francesco Zoccheddu <23437496+francescozoccheddu@users.noreply.github.com>
2021-06-02 12:43:36 +0200

    Use DistanceMeasure

--- app/image-classifier/src/main/scala/image_classifier/configuration/Loader.scala
+++ app/image-classifier/src/main/scala/image_classifier/configuration/Loader.scala
@@ -49,6 +49,9 @@ object Loader {
 	def loadOrMakeAndSave[Type <: LoadableConfig](file: String, config: Type): Loader[Type] =
 		create(LoadOrMakeAndSave, Some(file), Some(config))
 
+	def makeAndSave[Type <: LoadableConfig](config: Type, file: String): Loader[Type] =
+		create(MakeAndSave, Some(file), Some(config))
+
 	def create[Type <: LoadableConfig](mode: LoadMode, file: O[String], config: O[Type]): Loader[Type] = mode match {
 		case Make => Loader(config, None, None, None)
 		case MakeAndSave => Loader(config, None, file, None)
@@ -56,9 +59,6 @@ object Loader {
 		case LoadOrMake => Loader(config, file, None, None)
 		case Load => Loader(None, file, None, None)
 	}
-
-	def makeAndSave[Type <: LoadableConfig](config: Type, file: String): Loader[Type] =
-		create(MakeAndSave, Some(file), Some(config))
 
 	def load[Type <: LoadableConfig](file: String): Loader[Type] =
 		create(Load, Some(file), None)
--- app/image-classifier/src/main/scala/image_classifier/pipeline/featurization/BOWV.scala
+++ app/image-classifier/src/main/scala/image_classifier/pipeline/featurization/BOWV.scala
@@ -6,6 +6,7 @@ import image_classifier.utils.DataTypeImplicits._
 import org.apache.spark.ml.clustering.KMeans
 import org.apache.spark.ml.linalg.SQLDataTypes.VectorType
 import org.apache.spark.ml.linalg.{Vector, Vectors}
+import org.apache.spark.mllib.clustering.NearestNeighbor
 import org.apache.spark.sql.DataFrame
 import org.apache.spark.sql.functions._
 import org.apache.spark.sql.types.ArrayType
@@ -15,6 +16,7 @@ private[featurization] object BOWV {
 	val defaultOutputCol: String = colName("data")
 	private val centerCol: String = resColName("center")
 	private val distanceCol: String = resColName("distance")
+	private val idCol: String = resColName("id")
 
 	def createCodebook(data: DataFrame, inputCol: String, config: CodebookConfig): Seq[Vector] = {
 		data.schema.requireField(inputCol, VectorType)
@@ -31,10 +33,10 @@ private[featurization] object BOWV {
 		val centers = model.clusterCenters
 		val assignedCenters = if (config.assignNearest) {
 			val centersBroadcast = data.sparkSession.sparkContext.broadcast(centers)
-			val distance = udf((feature: Vector, centerIndex: Int) => Vectors.sqdist(feature, centersBroadcast.value(centerIndex)))
+			val distanceUdf = udf((feature: Vector, centerIndex: Int) => Vectors.sqdist(feature, centersBroadcast.value(centerIndex)))
 			val newCenters = model
 			  .transform(data)
-			  .withColumn(distanceCol, distance(col(inputCol), col(centerCol)))
+			  .withColumn(distanceCol, distanceUdf(col(inputCol), col(centerCol)))
 			  .groupBy(centerCol, inputCol)
 			  .min(distanceCol)
 			  .select(inputCol)
@@ -49,25 +51,18 @@ private[featurization] object BOWV {
 
 	def compute(data: DataFrame, codebook: Seq[Vector], inputCol: String, outputCol: String = defaultOutputCol): DataFrame = {
 		data.schema.requireField(inputCol, ArrayType(VectorType))
-		val codebookBroadcast = data.sparkSession.sparkContext.broadcast(codebook)
-		val featurize = udf((features: Seq[Vector]) => {
-			val neighbors = features.map(f => {
-				var minDist = Double.PositiveInfinity
-				var minI = 0
-				var i = codebookBroadcast.value.length
-				while (i > 0) {
-					i -= 1
-					val dist = Vectors.sqdist(f, codebookBroadcast.value(i))
-					if (dist < minDist) {
-						minDist = dist
-						minI = i
-					}
-				}
-				minI
-			})
-			Histogram.compute(neighbors, codebookBroadcast.value.length)
-		})
-		data.withColumn(outputCol, featurize(col(inputCol)))
+		val joiner = Joiner(codebook.toArray)
+		val joinUdf = udf(joiner.apply: Array[Vector] => Vector)
+		data.withColumn(outputCol, joinUdf(col(inputCol)))
+	}
+
+	private final case class Joiner(codebook: Array[Vector]) {
+
+		private val nn: NearestNeighbor = NearestNeighbor(codebook)
+
+		def apply(features: Array[Vector]): Vector =
+			Histogram.compute(features.map(nn.predict), codebook.length)
+
 	}
 
 }
