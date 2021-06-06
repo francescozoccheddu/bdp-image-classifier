46ce810123d1e3e75c0ab9c829eaab47ab66605c
Francesco Zoccheddu <23437496+francescozoccheddu@users.noreply.github.com>
2021-06-06 15:43:37 +0200

    Update download.sh

--- app/download.sh
+++ app/download.sh
@@ -6,7 +6,7 @@ OUTPUT=`realpath "${1:-assembly.jar}"`
 
 echo "-- Downloading '$OUTPUT'"
 
-wget "https://github.com/francescozoccheddu/Big-Data-Project/releases/download/0.0.1/assembly.jar" -O $OUTPUT -nv --show-progress || { echo "-- Failed"; exit 1; }
+wget "https://github.com/francescozoccheddu/Big-Data-Project/releases/download/v0.1-alpha/assembly.jar" -O $OUTPUT -nv --show-progress || { echo "-- Failed"; exit 1; }
 
 echo "-- Done"
 
