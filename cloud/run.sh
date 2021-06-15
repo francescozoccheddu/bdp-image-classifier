5de02dcb06c70e7ba1e464824970653392a2cb2d
Francesco Zoccheddu <23437496+francescozoccheddu@users.noreply.github.com>
2021-06-15 20:18:53 +0200

    Update cloud/run.sh

--- cloud/run.sh
+++ cloud/run.sh
@@ -38,7 +38,7 @@ done
 [ "$DATASET_OK" = true ] || dieh "Unknown dataset '$DATASET'."
 ( [ "$INSTANCE_COUNT" -ge 2 ] && [ "$INSTANCE_COUNT" -le 10 ] ) || dieh "INSTANCE_COUNT must fall in range [2, 10]."
 
-# CLI
+# AWS
 
 export AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID:-${ARGS[AWS_AK_ID]}}
 export AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY:-${ARGS[AWS_AK_SECRET]}}
@@ -48,21 +48,21 @@ export AWS_DEFAULT_OUTPUT="text"
 [ -n "$AWS_ACCESS_KEY_ID" ] || dieh "Missing AWS access key ID. Set the 'AWS_ACCESS_KEY_ID' environment variable or use the -k option."
 [ -n "$AWS_SECRET_ACCESS_KEY" ] || dieh "Missing AWS secret access key. Set the 'AWS_SECRET_ACCESS_KEY' environment variable or use the -s option."
 
-log "Setting up the AWS CLI"
+log "Setting up the AWS AWS"
 
-CLI="$SDIR/.aws-cli"
-if [ -f "$CLI" ]; then 
-	echo "Reusing cached AWS CLI."
+AWS="$SDIR/.aws-cli"
+if [ -f "$AWS" ]; then 
+	echo "Reusing cached AWS AWS."
 else
-	curl -o "$CLI" -L "https://github.com/simnalamburt/awscliv2.appimage/releases/latest/download/aws-x86_64.AppImage" || die "Download failed."
-	chmod +x "$CLI"
+	curl -o "$AWS" -L "https://github.com/simnalamburt/awscliv2.appimage/releases/latest/download/aws-x86_64.AppImage" || die "Download failed."
+	chmod +x "$AWS"
 fi
 
-UID=`"$CLI" sts get-caller-identity | cut -f1` || die "AWS authentication failed. Is the access key correct?"
+UID=`"$AWS" sts get-caller-identity | cut -f1` || die "AWS authentication failed. Is the access key correct?"
 
 CLUSTER_NAME="francescozoccheddu-big-data-project-image-classifier"
 
-"$CLI" emr list-clusters --active | grep "^CLUSTERS" | cut -f4 | grep "^$CLUSTER_NAME$" && die "An active cluster already exists."
+"$AWS" emr list-clusters --active | grep "^CLUSTERS" | cut -f4 | grep "^$CLUSTER_NAME$" && die "An active cluster already exists."
 
 echo "S3 and EMR resources will be allocated in region '$AWS_DEFAULT_REGION'."
 
@@ -80,15 +80,16 @@ function fin_cleanup {
 
 # Create bucket
 
-S3="s3://francescozoccheddu-big-data-project-image-classifier-$UID"
+BUCKET_NAME="francescozoccheddu-big-data-project-image-classifier-$UID"
+S3="s3://$BUCKET_NAME"
 
-log "Creating S3 bucket '$S3'"
-"$CLI" s3 mb "$S3" || die "Failed to create the bucket."
+log "Creating S3 bucket '$BUCKET_NAME'"
+"$AWS" s3 mb "$S3" || die "Failed to create the bucket."
 
 function fin_cleanup {
 	cleardir
 	log "Deleting the bucket"
-	"$CLI" s3 rb "$S3" --force || die "Failed to delete the bucket."
+	"$AWS" s3 rb "$S3" --force || die "Failed to delete the bucket."
 }
 
 # Upload scripts
@@ -97,22 +98,34 @@ S3_CONF="$S3/configuration.json"
 S3_SCRIPT="$S3/job.sh" 
 
 log "Uploading scripts"
-"$CLI" s3 cp "$SDIR/.job.sh" "$S3_SCRIPT" || die "Failed to upload scripts."
-"$CLI" s3 cp "$SDIR/.configuration.json" "$S3_CONF" || die "Failed to upload scripts."
+"$AWS" s3 cp "$SDIR/.job.sh" "$S3_SCRIPT" || die "Failed to upload scripts."
 
 # Create cluster
 
-log "Creating the cluster"
+log "Creating the cluster '$CLUSTER_NAME'"
 
+RES_NAME="results"
+LOGS_NAME="logs"
 S3_RES="$S3/results"
-S3_LOGS="$S3_RES/logs" 
 
-CLUSTER_ID=`"$CLI" create-cluster` || die "Failed to create the cluster."
+"$AWS" emr create-default-roles || die "Failed to create EMR default roles." 
+CLUSTER_ID=$("$AWS" create-cluster 
+	--applications Name=Spark 
+	--use-default-roles 
+	--auto-terminate 
+	--release-label emr-6.3.0 
+	--log-uri "s3n://$BUCKET_NAME/$RES_NAME/$LOGS_NAME" 
+	--name "$CLUSTER_NAME" 
+	--configurations "file://$SDIR/.configuration.json" 
+	--instance-type m4.large 
+	--instance-count $INSTANCE_COUNT`
+	--steps Type=CUSTOM_JAR,Name=Job,ActionOnFailure=TERMINATE_CLUSTER,Jar=s3://$AWS_DEFAULT_REGION.elasticmapreduce/libs/script-runner/script-runner.jar,Args=["$S3_SCRIPT","$DATASET"]
+	) || die "Failed to create the cluster."
 echo "Cluster created succesfully with ID '$CLUSTER_ID'"
 
 function fail_cleanup {
 	log "Terminating the cluster"
-	"$CLI" emr terminate-clusters --cluster-ids "$CLUSTER_ID" || die "Failed to terminate the cluster."
+	"$AWS" emr terminate-clusters --cluster-ids "$CLUSTER_ID" || die "Failed to terminate the cluster."
 }
 
 # Wait for cluster termination
@@ -120,7 +133,7 @@ function fail_cleanup {
 echo "Waiting for the cluster to terminate its job..."
 echo "Type CTRL+C to abort."
 
-"$CLI" emr wait cluster-terminated --cluster-id "$CLUSTER_ID" || die "Cluster timed out."
+"$AWS" emr wait cluster-terminated --cluster-id "$CLUSTER_ID" || die "Cluster timed out."
 
 echo "The cluster has terminated its job."
 
@@ -130,7 +143,7 @@ log "Collecting results"
 
 function fail_cleanup {
 	rm -f "$OUTPUT_DIR/model" "$OUTPUT_DIR/summary"
-	rm -rf "$OUTPUT_DIR/logs"
+	rm -rf "$OUTPUT_DIR/$LOGS_NAME"
 }
 
-"$CLI" s3 cp "$S3_RES" "$OUTPUT_DIR" --recursive || die "Failed to download results."
+"$AWS" s3 cp "$S3/$RES_NAME" "$OUTPUT_DIR" --recursive || die "Failed to download results."
