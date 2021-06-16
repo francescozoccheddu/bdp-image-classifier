922b1966907fe346620ef8eae593e7db52e8121a
Francesco Zoccheddu <23437496+francescozoccheddu@users.noreply.github.com>
2021-06-17 01:38:53 +0200

    Add app/download

---
+++ tools/app/download.py
@@ -0,0 +1,24 @@
+
+from .. import utils
+
+from enum import Enum
+
+class Architecture(Enum):
+	x64='x64'
+
+	def __str__(self) -> str:
+		return self.value
+
+def download(architecture: Architecture, output_file: str):
+	utils.download(f'https://github.com/francescozoccheddu/big-data-project/releases/download/v0.1-alpha/assembly-{architecture}.jar', output_file)
+
+def _main():
+	import argparse
+	parser = argparse.ArgumentParser(description='Download prebuilt app JARs', formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+	parser.add_argument('architecture', type=Architecture, choices=list(Architecture), help='the target architecture')
+	parser.add_argument('-o', '--output-file', type=utils.output_file_arg, default='assembly.jar', help='the output JAR file')
+	args = parser.parse_args()
+	download(args.architecture, args.output_file)
+
+if __name__ == '__main__':
+	_main()
