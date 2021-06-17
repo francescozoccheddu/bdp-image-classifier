3cdb699bb906f7b0ab6f056cbdd36b5065d14036
Francesco Zoccheddu <23437496+francescozoccheddu@users.noreply.github.com>
2021-06-17 17:23:00 +0200

    Add tools/datasets/reconfigure

--- tools/datasets/reconfigure.py
+++ tools/datasets/reconfigure.py
@@ -1,14 +1,83 @@
-default_print_summary = True
-default_data_temp_file = 'hdfs:///image-classifier/data'
-default_model_output_file = 'hdfs:///image-classifier/model'
-default_summary_output_file = 'hdfs:///image-classifier/summary'
-default_dataset_cwd = '.'
+from .. import utils
+
+_default_data_save = None
+_default_data_temp_file = 'hdfs:///image-classifier/data'
+_default_featurization_save = None
+_default_training_save = 'hdfs:///image-classifier/model'
+_default_testing_save = 'hdfs:///image-classifier/summary'
+_default_testing_print = True
+_default_data_cwd = '.'
+
+
+def _map_file_cwd(file, cwd):
+    from urllib.parse import urlparse
+    from os.path import isabs
+    uri = urlparse(file)
+    if uri.scheme or uri.netloc or isabs(uri.path):
+        return file
+    else:
+        return f'{cwd}/{file}'
+
+
+def _map_dataset_cwd(dataset, cwd):
+    for images in dataset:
+        for i, image in enumerate(images):
+            images[i] = _map_file_cwd(image, cwd)
 
 
 def reconfigure(
-        print_summary=True,
-        data_temp_file=default_data_temp_file,
-        model_output_file=default_model_output_file,
-        summary_output_file=default_summary_output_file,
-        dataset_cwd=default_dataset_cwd):
-    pass
+        file,
+        data_save=_default_data_save,
+        data_temp_file=_default_data_temp_file,
+        data_cwd=_default_data_cwd,
+        featurization_save=_default_featurization_save,
+        training_save=_default_training_save,
+        testing_save=_default_testing_save,
+        testing_print=_default_testing_print):
+    import json
+    with open(file) as f:
+        config = json.load(f)
+    data = config.get('data')
+    featurization = config.get('featurization')
+    training = config.get('training')
+    testing = config.get('testing')
+    if data is not None:
+        data['save'] = data_save
+        make = data.get('make')
+        if make is not None:
+            make['tempFile'] = data_temp_file
+            datasetKeys = ('dataSet', 'trainingSet', 'testSet')
+            for datasetKey in datasetKeys:
+                dataset = make.get(datasetKey)
+                if dataset is not None:
+                    _map_dataset_cwd(dataset, data_cwd)
+    if 'featurization' in config:
+        featurization['save'] = featurization_save
+    if 'training' in config:
+        training['save'] = training_save
+    if 'testing' in config:
+        testing['print'] = testing_print
+        testing['save'] = testing_save
+    with open(file, 'w') as f:
+        json.dump(config, f)
+
+
+def _main():
+    utils.hook_exceptions()
+    import argparse
+    from os import path
+    parser = argparse.ArgumentParser(description=f'Change output paths in a JSON configuration file', formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('config_file', metavar='CONFIG_FILE', type=utils.input_file_or_parent_argb('config.json'), help='the JSON config file or the dataset directory')
+    parser.add_argument('--data-save', type=str, default=_default_data_save, help='the data output file')
+    parser.add_argument('--data-temp-file', type=str, default=_default_data_temp_file, help='the data temporary file')
+    parser.add_argument('--data-cwd', type=str, default=_default_data_cwd, help='the datasets working directory')
+    parser.add_argument('--featurization-save', type=str, default=_default_featurization_save, help='the features output file')
+    parser.add_argument('--training-save', type=str, default=_default_training_save, help='the model output file')
+    parser.add_argument('--testing-save', type=str, default=_default_testing_save, help='the testing summary output file')
+    parser.add_argument('--testing-print', type=utils.bool_arg, default=_default_testing_print, help='whether to print the testing summary to stdout')
+    args = parser.parse_args()
+    reconfigure(args.config_file, args.data_save, args.data_temp_file, args.data_cwd, args.featurization_save, args.training_save, args.testing_save, args.testing_print)
+
+
+if __name__ == '__main__':
+    _main()
