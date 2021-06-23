635cc788cd7b38e13cf0907d05fb5d17c72f1a70
Francesco Zoccheddu <23437496+francescozoccheddu@users.noreply.github.com>
2021-06-23 12:17:58 +0200

    Change memory unit in tools/debug_env/run

--- tools/bdp_image_classifier/debug_env/run.py
+++ tools/bdp_image_classifier/debug_env/run.py
@@ -13,7 +13,7 @@ def run(install_dir, assembly_file, config_file, driver_ram_mb=2048, executor_ra
         if cli.run(start_script, [], enable_out=False, enable_err=False) != 0:
             raise RuntimeError('Failed to start HDFS daemons')
         cli.log('Running...')
-        cli.run(run_script, ['--master', 'local[*]', '--driver-memory', f'{driver_ram_mb}M', '--executor-memory', f'{executor_ram_mb}M', assembly_file, config_file], enable_out=True, enable_err=True)
+        cli.run(run_script, ['--master', 'local[*]', '--driver-memory', f'{driver_ram_mb}G', '--executor-memory', f'{executor_ram_mb}G', assembly_file, config_file], enable_out=True, enable_err=True)
     finally:
         cli.log('Stopping HDFS daemons...')
         if cli.run(stop_script, [], enable_out=False, enable_err=False) != 0:
@@ -29,8 +29,8 @@ def _main():
     cli.add_argparse_quiet(parser)
     parser.add_argument('assembly_file', metavar='ASSEMBLY_FILE', type=cli.input_file_arg, help='the app JAR file')
     parser.add_argument('config_file', metavar='CONFIG_FILE', type=cli.make_input_file_or_parent_arg('config.json'), help='the JSON configuration file')
-    parser.add_argument('--driver-ram', type=cli.make_int_arg(2**8, 2**15), default=2**10, help='the driver RAM amount in MB')
-    parser.add_argument('--executor-ram', type=cli.make_int_arg(2**8, 2**15), default=2**10, help='the executor RAM amount in MB')
+    parser.add_argument('--driver-ram', type=cli.make_int_arg(1, 128), default=1, help='the driver RAM amount in GB')
+    parser.add_argument('--executor-ram', type=cli.make_int_arg(1, 128), default=1, help='the executor RAM amount in GB')
     args = parser.parse_args()
     cli.set_exception_hook()
     cli.set_logging(not args.quiet)
